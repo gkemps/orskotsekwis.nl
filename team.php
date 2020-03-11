5a2bbcc3549a2cf729a8f47ff8cc8de523ebb2a5
geert <geert@route42.nl>
2020-03-11 21:13:40 +0100

    Include connect.php

--- team.php
+++ team.php
@@ -1,21 +1,7 @@
 <?php
 mb_internal_encoding("UTF-8");
 
-$servername = "localhost";
-$username = "orskotsekwis";
-$password = "";
-$dbname = "";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-if (!$conn->set_charset("utf8")) {
-    printf("Error loading character set utf8: %s\n", $conn->error);
-    exit();
-}
+include "connect.php";
 
 $pincode = $conn->real_escape_string($_GET['id'] ?? 999999);
 
