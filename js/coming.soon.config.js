c47b5427564d4a6b384e7cba21e0ad14d9a2d759
geert <geert@route42.nl>
2022-12-13 12:56:41 +0100

    timer to start of subscription

--- js/coming.soon.config.js
+++ js/coming.soon.config.js
@@ -3,7 +3,7 @@
 		// Set the time at which the countdown expires.
 		// var untilDate new Date(Year, Month - 1, Day)
 		//-----------------------------------------------
-		var untilDate = new Date(2022, 8, 17, 19);
+		var untilDate = new Date(2022, 11, 17, 00);
 
 		$(".countdown").countdown({
 			until: untilDate, 
