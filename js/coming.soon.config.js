1bc91db9d5ef0b7a4c2606bdac75b01ac733bf21
geert <geert@route42.nl>
2022-12-13 14:16:50 +0100

    countdown to quiz date, not subscription date

--- js/coming.soon.config.js
+++ js/coming.soon.config.js
@@ -3,7 +3,7 @@
 		// Set the time at which the countdown expires.
 		// var untilDate new Date(Year, Month - 1, Day)
 		//-----------------------------------------------
-		var untilDate = new Date(2022, 11, 17, 00);
+		var untilDate = new Date(2023, 2, 18, 19);
 
 		$(".countdown").countdown({
 			until: untilDate, 
